fd0fdff2d936bf93423dc67f13615f93477e4cc3
Dmitrii Belov <dmitrii.belov99@gmail.com>
2024-05-29 19:43:51 +0200

    feat: Add obstacles handling
    Ref: https://trello.com/c/zKHflasB

--- moonbot/domain/bot.py
+++ moonbot/domain/bot.py
@@ -29,10 +29,27 @@ class State(BaseModel):
     def __str__(self):
         return f"({self.x}, {self.y}) {self.direction.value}"
 
+    @property
+    def coords(self) -> tuple[int, int]:
+        return (self.x, self.y)
+
+
+class Status(Enum):
+    SUCCESS = "success"
+    STOPPED = "stopped"
+
 
 class Bot:
-    def __init__(self, x: int, y: int, direction: Direction):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        direction: Direction,
+        *,
+        obstacles: set[tuple[int, int]] | None = None,
+    ):
         self._state = State(x=x, y=y, direction=direction)
+        self._obstacles = obstacles or set()
 
     @property
     def state(self) -> State:
@@ -40,14 +57,17 @@ class Bot:
 
     def _get_next_state_after_moving(
         self, instruction: Instruction, state: State
-    ) -> State:
+    ) -> tuple[State, bool]:
         direction_coeff = (
             1 if state.direction in [Direction.NORTH, Direction.EAST] else -1
         )
         move_coeff = 1 if instruction == Instruction.FORWARD else -1
         coord = "x" if state.direction in [Direction.WEST, Direction.EAST] else "y"
         update = {coord: getattr(state, coord) + direction_coeff * move_coeff}
-        return State(**update, **state.model_dump(exclude={coord}))
+        new_state = State(**update, **state.model_dump(exclude={coord}))
+        if new_state.coords in self._obstacles:
+            return (state, False)
+        return (new_state, True)
 
     def _get_next_state_after_rotating(
         self, instruction: Instruction, state: State
@@ -58,17 +78,21 @@ class Bot:
         new_direction = directions[new_index % len(directions)]
         return State(direction=new_direction, **state.model_dump(exclude={"direction"}))
 
-    def move(self, command: str) -> None:
+    def move(self, command: str) -> Status:
         for letter in command:
             if letter not in Instruction:
                 raise InvalidCommand(f'"{letter}" is not a valid command')
             instruction = Instruction(letter)
             match instruction:
                 case Instruction.FORWARD | Instruction.BACK:
-                    self._state = self._get_next_state_after_moving(
+                    self._state, moved = self._get_next_state_after_moving(
                         instruction, self._state
                     )
+                    if not moved:
+                        return Status.STOPPED
+
                 case Instruction.LEFT | Instruction.RIGHT:
                     self._state = self._get_next_state_after_rotating(
                         instruction, self._state
                     )
+        return Status.SUCCESS
--- tests/unit/test_bot.py
+++ tests/unit/test_bot.py
@@ -1,7 +1,7 @@
 import pytest
 from pydantic import ValidationError
 
-from moonbot.domain.bot import Bot, Direction, State
+from moonbot.domain.bot import Bot, Direction, State, Status
 from moonbot.domain.exceptions import InvalidCommand
 
 
@@ -9,9 +9,10 @@ def test_bot_returns_current_state(bot: Bot):
     assert bot.state == State(x=1, y=2, direction=Direction.WEST)
 
 
-def test_bot_can_move(bot: Bot):
-    bot.move("FLFFFRFLB")
+def test_bot_can_move_and_returns_status_success(bot: Bot):
+    status = bot.move("FLFFFRFLB")
     assert bot.state == State(x=-1, y=0, direction=Direction.SOUTH)
+    assert status == Status.SUCCESS
 
 
 @pytest.mark.parametrize(
@@ -29,3 +30,10 @@ def test_validation_error_is_raised_on_invalid_constructor_arguments(args):
 def test_validation_error_is_raised_on_invalid_move_command(bot):
     with pytest.raises(InvalidCommand):
         bot.move("FLFINVALID")
+
+
+def test_stops_before_an_obstacle_and_returns_status_stopped():
+    bot = Bot(0, 0, Direction.NORTH, obstacles={(0, 2), (2, 3)})
+    status = bot.move("FFFLR")
+    assert bot.state == State(x=0, y=1, direction=Direction.NORTH)
+    assert status == Status.STOPPED
